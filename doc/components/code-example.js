0ea727c8ca6d016e0968761dbd9f7be8ec8630e1
Yannick Bochatay <yannick.bochatay@free.fr>
2026-03-15 09:29:06 +0100

    create accessible code web component

--- doc/components/code-example.js
+++ doc/components/code-example.js
@@ -1,23 +1,32 @@
-import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/es/highlight.min.js";
-import javascript from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/es/languages/javascript.min.js";
-import css from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/es/languages/css.min.js";
-
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('css', css);
+import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/es/highlight.min.js"
 
 const template = document.createElement("template");
 
 template.innerHTML = `
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-light.min.css"
-  >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-light.min.css">
   <style>
-    code {
-      background-color:var(--foreground);
+    :host {
+      display:block;
+
+      & > div {
+        overflow-x:auto;
+
+        pre {
+          margin:0;
+
+          code.hljs {
+            box-sizing:border-box;
+            min-width:100%;
+            width:fit-content;
+          }
+        }
+      }
     }
   </style>
-  <pre><code class="language-xxx" tabindex="0"></code></pre>
+  <div tabindex="0">
+    <pre><code class="language-xxx"></code></pre>
+  </div>
+  <slot></slot>
 `
 
 class CodeExample extends HTMLElement {
@@ -33,11 +42,12 @@ class CodeExample extends HTMLElement {
   }
 
   connectedCallback() {
-    const content = this.innerHTML.trim();
-    const highlightedCode = hljs.highlight(content, { language: this.language }).value;
-    const code = this.shadowRoot.querySelector("code");    
+    const code = this.shadowRoot.querySelector("code");
+    const slot = this.shadowRoot.querySelector("slot");    
     code.className = code.className.replace("xxx", this.language);
-    code.innerHTML = highlightedCode;
+    code.append(...slot.assignedNodes());
+    slot.remove();
+    hljs.highlightElement(code);
   }
 }
 
